e372006221ae798d79f8a2e74d080ee2767d2c39
BENJAMIN DAVIES <bendavies0804@gmail.com>
2023-03-07 16:44:07 +0000

    Updates

--- CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
+++ CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -9,10 +9,22 @@ namespace CMP1903M_A01_2223
     class Card
     {
         //Base for the Card class.
+
+        
         //Value: numbers 1 - 13
+        List<int> Value = new List<int>() {1,2,3,4,5,6,7,8,9,10,11,12,13};
         //Suit: numbers 1 - 4
+        List<int> Suit = new List<int>() {1,2,3,4};
+
         //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+       public int value { get; set; }
+       public int suit { get; set; }
+
+        public Card(int Value, int Suit)
+        {
+            Value = Value;
+            Suit = Suit;  
+
+        }
     }
 }
--- CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -12,12 +12,33 @@ namespace CMP1903M_A01_2223
 
         public Pack()
         {
-            //Initialise the card pack here
+            pack = new List<Card>();
+            for(int i = 1; i <=4; i++)
+            {
+                for(int j = 1; j <=13; j++)
+                {
+                    Card card = new Card(i,j);
+                    pack.Add(card);
+                }
+            }
         }
 
         public static bool shuffleCardPack(int typeOfShuffle)
         {
-            //Shuffles the pack based on the type of shuffle
+            if (typeOfShuffle == 1)
+            {
+                //FiSHER YATES
+            }  
+            else if (typeOfShuffle == 2)
+            {
+                //RIFFLE
+            }
+            else
+            {
+                //no shuffle
+            }
+            //Shuffles the pack based on the type of shuffle   
+            return true;
 
         }
         public static Card deal()
