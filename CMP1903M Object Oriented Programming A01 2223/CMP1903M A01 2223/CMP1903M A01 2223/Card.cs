4faa8fe55b0a39b6e12ab7e6f007cfbb6243a39d
BENJAMIN DAVIES <bendavies0804@gmail.com>
2023-03-09 00:01:11 +0000

    Changed loads including a working fisher yates shuffle

--- CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
+++ CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -7,47 +7,86 @@ using System.Threading.Tasks;
 namespace CMP1903M_A01_2223
 {
 
-    public enum Value
-    {
-        Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
-    };
-    public enum Suit
-    {
-        Spades,
-        Hearts,
-        Diamonds,
-        Clubs
-    };
     class Card
     {
+
+        /*
         //Base for the Card class.
 
 
         //Value: numbers 1 - 13
         
-        //public enum Value 
-        //{ 
-        //    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King 
-        //};
+        public enum Value 
+        { 
+            Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King 
+        };
         //Suit: numbers 1 - 4
-        //public enum Suit 
-        //{ 
-        //    Spades, 
-        //    Hearts, 
-        //    Diamonds, 
-        //    Clubs 
-        //};
+        public enum Suit 
+        { 
+            Spades, 
+            Hearts, 
+            Diamonds, 
+            Clubs 
+        };
 
-
+        
         //The 'set' methods for these properties could have some validation
-        public Value _value { get; set; }
-        public Suit _suit { get; set; }
+   
 
-        public Card(Value value, Suit suit)
+        public Card(Value Value, Suit Suit)
         {
-            _value = value;
-            _suit = suit; 
+            Value = value;
+            Suit = suit; 
 
+        }
+        */
+
+        public int Value { get; set; }
+        public string Suit { get; set; }
+        public Card(int val)
+        {
+            // Generating the corresponding cards to values and suits
+            Value = (val % 13) + 1;
+            if (val / 13 == 0)
+            {
+                Suit = "Diamonds";
+            }
+            else if (val / 13 == 1)
+            {
+                Suit = "Clubs";
+            }
+            else if (val / 13 == 2)
+            {
+                Suit = "Hearts";
+            }
+            else
+            {
+                Suit = "Spades";
+            }
+        }
+        // Custom added method to display Ace, Jack, Queen or King as well as making the numbers in the same format
+        public string Display()
+        {
+            if (Value == 1)
+            {
+                return "Ace of " + Suit;
+            }
+            else if (Value == 11)
+            {
+                return "Jack of " + Suit;
+            }
+            else if (Value == 12)
+            {
+                return "Queen of " + Suit;
+            }
+            else if (Value == 13)
+            {
+                return "King of " + Suit;
+            }
+            else
+            {
+                return Value + " of " + Suit;
+            }
         }
     }
 }
--- CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,32 +14,42 @@ namespace CMP1903M_A01_2223
 
         public Pack()
         {
-            List<Card> pack = new List<Card>();
-            Pack deck = new Pack();
-            for (int i = 0; i < 4; i++)
+            pack = new List<Card>();             //Creates a list
+
+            for (int i = 0; i < 52; i++)
             {
-                for(int j = 0; j < 13; j++)
-                {
-                    pack.Add(new Card((Value)i, (Suit)j));
-                }
+                pack.Add(new Card(i));          //Adds all 52 cards to the deck (list)
             }
-            Console.WriteLine(String.Join(", ", pack));
+
         }
         public static bool shuffleCardPack(string typeOfShuffle)
         {
- 
 
-            if (typeOfShuffle == "1")
+            if (typeOfShuffle == "1")                   //If the user types 1, fisher yates shuffle starts
             {
 
-                Console.Write("Fisher yates shuffle chosen");
-                Console.ReadLine();
-                //Console.WriteLine(String.Join(", ", Pack.deck));
-
-            }  
+                Console.WriteLine("Fisher Yates shuffle chosen");
+                Pack pack = new Pack();
+                Random random = new Random();
+                List<Card> shuffledPack = new List<Card>();
+                for (int i = 52; i > 0; i--)
+                {
+                    int x = random.Next(i);
+                    var temp = pack.pack[x];                    //Performs the fisher yates shuffle
+                    shuffledPack.Add(temp);
+                    pack.pack.RemoveAt(x);
+                
+                }
+                pack.pack = shuffledPack;
+                deal(pack.pack);                                //Deals the card after the shuffle
+                return true;
+                //Console.ReadLine();
+            }
             else if (typeOfShuffle == "2")
             {
                 Console.Write("Riffle shuffle chosen");
+                Pack pack = new Pack();
+                List<Card> shuffledPack = new List<Card>();                 
                 Console.ReadLine();
             }
             else
@@ -50,12 +62,19 @@ namespace CMP1903M_A01_2223
 
         }
 
-        //public static Card deal()
-        //{
+        public static Card deal(List<Card> list)
+        {
             //Deals one card
-
-        //}
-        //public static List<Card> dealCard(int amount)
+            var deal = list.Take(1);
+            deal = list.Take(1);
+            foreach(Card card in deal)
+            {
+                Console.WriteLine(card.Value + " of " + card.Suit);
+                Console.ReadLine();
+            }
+            return deal.First();
+        }
+       //public static List<Card> dealCard(int amount)
         //{
             //Deals the number of cards specified by 'amount'
         //}
