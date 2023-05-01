5ecabc034f4780f6ef3ce9c459338f66e719d270
BENJAMIN DAVIES <bendavies0804@gmail.com>
2023-05-01 13:01:33 +0100

    Further with Assignment 2start of assessment 2

---
+++ CMP1903M Object Oriented Programming A01 2223/CMP1903M A01 2223/CMP1903M A01 2223/Tutorial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Tutorial
+    {
+        public static void Menu()
+        {
+            bool valid = true;
+            while (valid)
+            {
+
+                Console.Write("Choose a shuffle \n\t\t ");
+                Console.Write("1 -- Fisher Yates || 2 -- Riffle || 3 -- no shuffle:  ");
+                string typeOfShuffle = Console.ReadLine();
+                if (Convert.ToInt32(typeOfShuffle) > 0 && Convert.ToInt32(typeOfShuffle) < 4)
+                {
+                    Pack.shuffleCardPack(typeOfShuffle);                                                //This while loop will only allow the user to put in a number between 1 and 3 for the shuffles.
+                    valid = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid shuffle chosen, try again.");
+                }
+
+
+            }
+        }
+
+    }
+}
